cf746fc3741267ea597b8abdb0b7efe6d5b9727c
xiashasha <2603925038@qq.com>
2020-05-12 22:45:14 +0800

    blog demo

--- JAVA13/io-study/src/lesson1/Main.java
+++ JAVA13/io-study/src/lesson1/Main.java
@@ -13,21 +13,33 @@ public class Main{
        return 0;
     }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String[] P = sc.next().split("\\.");
+            String[] A = sc.next().split("\\.");
+            for(String s:P){
+                System.out.println(s);
+            }
+            System.out.println(P.length);
+            int[] arrP = new int[3];
+            int[] arrA = new int[3];
+            for(int i=0;i<3;i++){
+                arrP[i] = Integer.parseInt(P[i]);
+                arrA[i] = Integer.parseInt(A[i]);
+            }
+            int sumA = ((arrA[0]*17)+arrA[1])*19+arrA[2];
 
+            int sumP =  ((arrP[0]*17)+arrP[1])*19+arrP[2];
 
-//        Scanner sc = new Scanner(System.in);
-//
-//        while(sc.hasNextInt()){
-//            List<Integer> l=new LinkedList<>();
-//            int len = sc.nextInt();
-//
-//            for(int i=0; i<len; i++){
-//                l.add(i);
-//            }
-//            System.out.println(ret(l));
-//        }
-        Map<String,String> map = new LinkedHashMap<>();
+            System.out.println(sumA+" "+sumP);
+            int rest = sumA - sumP;
+            int G = rest/(17*19);
+            rest-= G*17*19;
+            int S = rest/19;
+            rest-= S*19;
+            int K = rest;
+            System.out.println(G+"."+S+"."+K);
 
-
+        }
     }
 }

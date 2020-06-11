1e716aac95abc40cf28a2481bd338ac2f6a71e6f
xiashasha <2603925038@qq.com>
2020-06-11 21:47:39 +0800

    ee-study

--- javaee-study/ee-study/src/main/java/free/config/MyConfig.java
+++ javaee-study/ee-study/src/main/java/free/config/MyConfig.java
@@ -1,15 +1,10 @@
 package free.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class MyConfig implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())//所有匹配的路径都会进入拦截器
-                .addPathPatterns("/**")//**代表所有子路径。*代表一级路径
-                .excludePathPatterns("/login5");//排除路径
-    }
+public class MyConfig  {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginInterceptor())//所有匹配的路径都会进入拦截器
+//                .addPathPatterns("/**")//**代表所有子路径。*代表一级路径
+//                .excludePathPatterns("/login5");//排除路径
+//    }
 }
--- javaee-study/ee-study/src/main/java/free/controller/LoginController.java
+++ javaee-study/ee-study/src/main/java/free/controller/LoginController.java
@@ -4,68 +4,72 @@ import free.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Date;
 
 @Controller
+@RequestMapping("/1")
 public class LoginController {
     @RequestMapping(value="/login")
-    public String login(Integer i){
-        if(i == 1){
-            return "redirect:/index.html";
-        }else{
-            return "forward:/index.html";
-        }
-
+    public String login(){
+//        if(i == 1){
+//            return "redirect:/index.html";
+//        }else{
+//            return "forward:/index.html";
+//        }
+    return "/login.html";
     }
 
-    @RequestMapping(value="/login1",method = RequestMethod.POST)
+
+    @RequestMapping(value="/login2",method = RequestMethod.POST)
     @ResponseBody
-    public Object login2(@RequestParam String username,String password){
+    public Object login2(@RequestParam String username,String password,String password2){
         User user = new User();
-        user.setUsername("ooo");
-        user.setPassword("111");
+        System.out.println(username+password+password2);
+        user.setUsername("光辉女郎");
+        user.setPassword("123");
         user.setBirthday(new Date());
         return user;
     }
-    @RequestMapping(value="/login2",method = RequestMethod.POST)
+    @RequestMapping(value="/login3",method = RequestMethod.POST)
     @ResponseBody
+    //接受fromdate和xxx-from-encdoing格式请求数据
     public Object login3(User user){
         User user1 = new User();
-        user1.setUsername(user.getUsername());
-        user1.setPassword(user.getPassword());
-        user1.setBirthday(user.getBirthday());
+        System.out.println(user);
+        System.out.println(user);
+        user1.setUsername("光辉女郎");
+        user1.setPassword("123");
+        user1.setBirthday(new Date());
         return user1;
     }
     @RequestMapping(value="/login4",method = RequestMethod.POST)
     @ResponseBody
-    public Object login4(@RequestBody  User user){
-        System.out.println(user);
-
-//        user.setUsername("小龙虾");
-//        user.setPassword("123");
-//        user.setBrithday(new Date());
+    //接受json格式的请求数据
+    public Object login4(@RequestBody User u){
+        System.out.println(u);
+        User user = new User();
+        user.setUsername("小龙虾");
+        user.setPassword("123");
+        user.setBirthday(new Date());
         return user;
     }
-    @RequestMapping(value="/login5",method = RequestMethod.POST)
-    @ResponseBody
-    public Object login5(HttpServletRequest request, HttpServletResponse response,
-                         User u){
-
-        if("abc".equals(u.getUsername()) && "123".equals(u.getPassword())) {
-            HttpSession session = request.getSession(false);
-            session.setAttribute("user",u);
-            User user = new User();
-            user.setUsername("小龙虾");
-            user.setPassword("123");
-            user.setBirthday(new Date());
-            return user;
-        }
-
-        return new RuntimeException("登录不成功");
-
-
-    }
+//    @RequestMapping(value="/login5",method = RequestMethod.POST)
+//    @ResponseBody
+//    public Object login5(HttpServletRequest request, HttpServletResponse response,
+//                         User u){
+//
+//        if("abc".equals(u.getUsername()) && "123".equals(u.getPassword())) {
+//            HttpSession session = request.getSession(false);
+//            session.setAttribute("user",u);
+//            User user = new User();
+//            user.setUsername("小龙虾");
+//            user.setPassword("123");
+//            user.setBirthday(new Date());
+//            return user;
+//        }
+//
+//        return new RuntimeException("登录不成功");
+//
+//
+//    }
 }
